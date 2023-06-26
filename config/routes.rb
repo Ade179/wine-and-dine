52bec0b1b0413851b98f7d0fb973bb80389738f5
Juwon <hardejuwon@gmail.com>
2023-06-26 22:19:22 +0100

    food controller and view now set

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   devise_for :users
-  root "foods#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "foods#index"
+
+  resources :foods
 end
