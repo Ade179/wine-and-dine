6007c88e0e23eb9fe63cf78c351c04f5e9d310f1
shasherazi <hassanrandomz@gmail.com>
2023-06-30 13:25:37 +0500

    Add mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Use sendinblue for email delivery
+  config.action_mailer.default_url_options = { host: 'wine-and-dine.onrender.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp-relay.sendinblue.com',
